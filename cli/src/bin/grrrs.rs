85c7e8f56464d5c7f9de2c88670ff44b424a7ee3
Cody Averett <codyaverett@gmail.com>
2023-01-31 09:13:19 -0600

    cli: add clap argument parser

--- cli/src/bin/grrrs.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    // following https://rust-cli.github.io/book/tutorial/cli-args.html
-}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // Note: PathBuf is like a String but for file system paths that work cross-platform.
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    // We can use the `args` variable to access the arguments passed to the program.
+    println!("Looking for {} in {}", args.pattern, args.path.display());
 }
