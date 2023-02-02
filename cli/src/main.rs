707d70df801ce8979eef26e10ffc868712105807
Cody Averett <codyaverett@gmail.com>
2023-02-01 23:16:45 -0600

    cli: read file specified by args

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,4 +13,12 @@ fn main() {
 
     // We can use the `args` variable to access the arguments passed to the program.
     println!("Looking for {} in {}", args.pattern, args.path.display());
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
